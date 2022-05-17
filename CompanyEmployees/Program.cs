9153c2dc4492dab0456b52c22abc6fc1785633ca
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-05-16 22:23:05 -0400

    incluye manejo de controllers y método get para companies

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -17,7 +17,8 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly); 
 
 var app = builder.Build();
 
